fd58271e1f3f00eb49e2bd45e05a24a44e7771ba
Jizhou Zhang <rectcrea@gmail.com>
2023-09-13 22:54:12 -0700

    complete flammable gas sensor gm-402b

--- temperature_humidity_air_gas_sensor/gm-402b.py
+++ temperature_humidity_air_gas_sensor/gm-402b.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 # datasheet: https://datasheet.lcsc.com/lcsc/1811061516_Zhengzhou-Winsen-Elec-Tech-GM-402B_C99754.pdf
+# datasheet ref2: https://datasheet.lcsc.com/lcsc/1811061516_Zhengzhou-Winsen-Elec-Tech-MQ-4_C99756.pdf
 
 import time
 import requests
@@ -22,24 +23,31 @@ def single_read_gas_level():
         "actions": [["adc", ADC_PIN, "3.1v"]]
     }
     r = requests.post(url, data=json.dumps(request_data)).json()
-    print(f"adc: {r}")
-    return 0
+    adc_voltage = r["result"][0][0] / 4096 * 3.1
+    return adc_voltage
 
 def main(args):
     if args.file_name != None:
         # empty file
         open(args.file_name, "w")
         while True:
-            air_pollute_val = single_read_gas_level()
+            flammable_gas_value = single_read_gas_level()
             pacific_tz = timezone("US/Pacific")
             time_str = datetime.now(
                 tz=pacific_tz).strftime("%m/%d/%Y %H:%M:%S")
-            air_pollute_log = f"{time_str} : {air_pollute_val}\r\n"
+            flammable_gas_log = f"{time_str} : {flammable_gas_value}\r\n"
             with open(args.file_name, "a") as f:
-                f.write(air_pollute_log)
+                f.write(flammable_gas_log)
             time.sleep(args.interval)
     else:
-        air_pollute_val = single_read_gas_level()
+        flammable_gas_value = single_read_gas_level()
+        print(f"flammable gas adc reading: {flammable_gas_value}v")
+        if flammable_gas_value < 1:
+            print("flammable gas level: LOW")
+        elif flammable_gas_value < 2:
+            print("flammable gas level: MID")
+        else:
+            print("flammable gas level: HIGH")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -48,10 +56,10 @@ if __name__ == "__main__":
         )
     )
     parser.add_argument(
-        "--file-name", "-f", type=pathlib.Path, default=None, help="Saving temperature to the file"
+        "--file-name", "-f", type=pathlib.Path, default=None, help="Saving flammable gas reading to the file"
     )
     parser.add_argument(
-        "--interval", "-i", type=int, default=10, help="Time interval to read temperature in second"
+        "--interval", "-i", type=int, default=10, help="Time interval to read flammable gas value in second"
     )
     args = parser.parse_args()
     main(args)
