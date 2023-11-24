bc65b9d0da5bf7334cdb46b54bfd3b6b5feac4a2
Jizhou Zhang <jz1958@nyu.edu>
2023-11-23 19:32:00 -0800

    small ultrasonic feature update

--- ultrasonic/distance_box.py
+++ ultrasonic/distance_box.py
@@ -3,6 +3,8 @@ import sys
 import json
 import time
 import argparse
+import datetime
+from enum import Enum
 
 PLATFORM_IP = "192.168.1.106"
 ULTRASONIC_PIN = 0
@@ -10,6 +12,14 @@ LED_STRIP_PIN = 4
 MAX_PAYLOAD_LEN = 8000
 NUMBER_LEDS = 26
 ULTRASONIC_CAPTURE_NUMBER_LOOPS = 15
+ULTRASONIC_THRESHOLD_MIDDLE = 14000
+ULTRASONIC_THRESHOLD_CLOSE = 3000
+STABLE_COUNT_VAL = 2        # 2 consecutive
+
+class LedState(Enum):
+    LED_OFF = 1
+    LED_RED = 2
+    LED_BLINK = 3
 
 def read_ultrasonic(platform_ip, echo_pin_id, trigger_pin_id):
     url = f"http://{platform_ip}/hardware/operation"
@@ -18,7 +28,7 @@ def read_ultrasonic(platform_ip, echo_pin_id, trigger_pin_id):
         "actions": [["capture", echo_pin_id, 10, "us", "change", 0.1, "trigger", trigger_pin_id, "high", "us", 10]]
     }
     result = requests.post(url, data=json.dumps(request_data)).json()["result"]
-    print(f"echo length: {result[0][2]}")
+    print(f"time: {datetime.datetime.now()} echo length: {result[0][2]}")
     return result[0][2]
 
 def led_strip_setup(platform_ip, led_pin_id):
@@ -69,20 +79,44 @@ LED_COLOR_SCHEMA = [
 ]
 
 def main(ip, ultrasonic_pin, led_pin):
-    led_on = False
+    led_state = False
+    far_stable_counter = 0
+    middle_stable_counter = 0
+    close_stable_counter = 0
+
     led_strip_setup(ip, led_pin)
     while True:
         echo_us = read_ultrasonic(ip, ultrasonic_pin, ultrasonic_pin)
-        if echo_us > 10000:
-            if led_on:
-                led_strip_color(ip, led_pin, NUMBER_LEDS, 0, 0, 0)
-                time.sleep(0.5)
-                led_on = False
+
+        if echo_us > ULTRASONIC_THRESHOLD_MIDDLE:
+            far_stable_counter += 1
+            if far_stable_counter >= STABLE_COUNT_VAL:
+                middle_stable_counter = 0
+                close_stable_counter = 0
+                if led_state != LedState.LED_OFF:
+                    led_strip_color(ip, led_pin, NUMBER_LEDS, 0, 0, 0)
+                    time.sleep(0.5)
+                    led_state = LedState.LED_OFF
+        elif echo_us > ULTRASONIC_THRESHOLD_CLOSE:
+            middle_stable_counter += 1
+            if middle_stable_counter >= STABLE_COUNT_VAL:
+                far_stable_counter = 0
+                close_stable_counter = 0
+                if led_state != LedState.LED_RED:
+                    led_strip_color(ip, led_pin, NUMBER_LEDS, 30, 0, 0)
+                    time.sleep(0.5)
+                    led_state = LedState.LED_RED
         else:
-            if not led_on:
-                led_strip_color(ip, led_pin, NUMBER_LEDS, 20, 0, 0)
-                time.sleep(0.5)
-                led_on = True
+            close_stable_counter += 1
+            if close_stable_counter >= STABLE_COUNT_VAL:
+                far_stable_counter = 0
+                middle_stable_counter = 0
+                led_strip_color(ip, led_pin, NUMBER_LEDS, 0, 20, 0)
+                time.sleep(0.4)
+                led_strip_color(ip, led_pin, NUMBER_LEDS, 20, 20, 0)
+                time.sleep(0.4)
+                led_state = LedState.LED_BLINK
+
 
 
 
